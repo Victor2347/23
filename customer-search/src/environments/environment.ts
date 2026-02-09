6a0f99f1bc8724731db571b617b825dd0a145629
Victor <snake2014828@gmail.com>
2026-02-09 08:51:26 +0800

    20260209

--- customer-search/src/environments/environment.ts
+++ customer-search/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // TODO: 請填入你的 Supabase URL 和 Anon Key
-  supabaseUrl: 'https://omvlxopmvfmbbodnwglb.supabase.co',
-  supabaseAnonKey: 'sb_publishable_YCCpOziygKJ_nIBBPKDKxQ_1LHxzW1v',
+  supabaseUrl: 'https://sovycaaalnlcftwaaiya.supabase.co',
+  supabaseAnonKey: 'sb_publishable_j2fIGTwraVRbj1oaT_uGZg_H8_Cj9eN',
 };
 
